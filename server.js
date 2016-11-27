3601eadad1386e9c9697da253a4a808ed9e7ca77
Rui Jiang <curraheejerry@gmail.com>
2016-11-27 17:43:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var Converter = require('csvtojson').Converter;
+var jsonfile = require('jsonfile')
+
+var app = express();
+
+var csvConverter = new Converter({})
+csvConverter.on('end_parsed', function(json) {
+  jsonfile.writeFile('public/dataset.json', json, function(err) {
+    if (err) {
+      throw err;
+    }
+    console.log('JSON file saved.');
+  });
+});
+
+fs.createReadStream('dataset.csv').pipe(csvConverter);
+
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Listening to port ' + port);
+});
