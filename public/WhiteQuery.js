baeb6dda97ec4494d09eb1a3bbca854292652bb7
siweiWang <swan357@emory.edu>
2016-11-28 00:33:49 -0500

    siwei 4

--- public/WhiteQuery.js
+++ public/WhiteQuery.js
@@ -211,7 +211,24 @@ function drawHistogram(data,type) {
     labels : label,
     datasets: [
         {
-            data: data
+            data: data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1,
         }
     ]
   };
@@ -220,6 +237,8 @@ function drawHistogram(data,type) {
     type: 'bar',
     data: model
   });
+  myChart.options.title.text = "top of chart";   // test, total fail
+  myChart.options.legend.display = false;
 
 }
 /**
